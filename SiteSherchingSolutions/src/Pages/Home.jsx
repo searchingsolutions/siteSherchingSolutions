2a536261a35e5c261a58606b66e903de6a36e9e3
Eliabr Rodrigues <eliabe859@gmail.com>
2024-09-13 13:42:25 -0300

    feat: Teste

--- SiteSherchingSolutions/src/Pages/Home.jsx
+++ SiteSherchingSolutions/src/Pages/Home.jsx
@@ -13,6 +13,7 @@ export function Home() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>Iae</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
