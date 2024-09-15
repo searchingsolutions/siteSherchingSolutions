f1a3de8f3b2608024aee4f746f64e2ab674b9fcd
Eliabr Rodrigues <eliabe859@gmail.com>
2024-09-15 15:18:12 -0300

    feat: Created navBar

--- SiteSherchingSolutions/src/Pages/Home.jsx
+++ SiteSherchingSolutions/src/Pages/Home.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
 import NavBar from '../layout/NavBar'
 import style from './Home.module.css'
-import logo from '../assets/background.svg'
+
 
 export function Home() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className={style.home}>
-        <NavBar/>
+    <div className={`${style.home}`}>
+        <header>
+          <NavBar/>
+        </header>
+       
     </div>
    
   )
